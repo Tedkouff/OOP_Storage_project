72827cbb30bd9d0a5755c613de1dbb0ae468eb5e
Teodor Kehayov <56166346+Tedkouff@users.noreply.github.com>
2022-05-09 22:13:10 +0300

    Update Product.h

--- Product.h
+++ Product.h
@@ -2,34 +2,36 @@
 #define PRODUCT_H
 
 #include <iostream>
+#include "MyString.h"
 #include "Date.h"
+#include "Location.h"
 
 class Product
 {
-    char* productName;
+    MyString productName;
     Date expireDate;
     Date entryDate;
-    char* manufacturerName;
+    MyString manufacturerName;
     std::size_t quantity;
     Location productLocation;
-    char* description;
+    MyString description;
 
+    Product& copy(const Product& rhs);
 public:
-    Product(const char* productName, const Date expireDate, const Date entryDate, const char* manufacturerName,
-            const std::size_t& quantity, const Location& productLocation, const char* comment);
+    Product(const MyString& productName, const Date& expireDate, const Date& entryDate, const MyString& manufacturerName,
+            const std::size_t& quantity, const Location& productLocation, const MyString& description);
     Product(const Product& rhs);
     Product& operator=(const Product& rhs);
 
-    char* getProductName() const;
+    MyString getProductName() const;
     Date getExpireDate() const;
     Date getEntryDate() const;
-    char* getManufacturerName() const;
+    MyString getManufacturerName() const;
     std::size_t getQuantity() const;
     Location getProductLocation() const;
-    char* getDescription() const;
+    MyString getDescription() const;
 
     friend std::ostream& operator<<(std::ostream& os, const Product& product);
-    friend std::istream& operator>>(std::istream& is, Product& product);
 };
 
 #endif //PRODUCT_H
