279980b89da66d1a13d444c74ef4a0d4612bd955
Teodor Kehayov <56166346+Tedkouff@users.noreply.github.com>
2022-05-09 22:16:57 +0300

    Create a light version of <string> class

---
+++ Light_String_Class/MyString.h
@@ -0,0 +1,35 @@
+#ifndef MYSTRING_H
+#define MYSTRING_H
+
+#include <iostream>
+#include <cstring>
+#include <stdexcept>
+
+
+class MyString
+{
+private:
+    char* str;
+    std::size_t lenght;
+
+public:
+    MyString();
+    MyString(const char* str);
+    ~MyString();
+    MyString(const MyString& str);
+    MyString& operator= (const MyString& str);
+    char& operator[](std::size_t pos);
+    const char& operator[](std::size_t pos) const;
+    
+    MyString& operator+=(const MyString& rhs);
+    MyString operator+(const MyString& rhs) const;
+    const char* c_str() const;
+    bool operator==(const MyString &rhs) const;
+    bool operator== (const char* str) const;
+
+    friend std::ostream& operator<< (std::ostream& os, const MyString& str);
+    friend std::istream& operator>> (std::istream& is, MyString &str);
+};
+
+
+#endif //MYSTRING_H
