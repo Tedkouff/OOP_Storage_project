be3451129f41c00f1106b40a390b8a08083492f0
Teodor Kehayov <56166346+Tedkouff@users.noreply.github.com>
2022-05-12 09:38:31 +0300

    Update Storage structure idea

--- Storage.h
+++ Storage.h
@@ -6,24 +6,24 @@
 class Storage
 {
 private:
-    Product*** products;
-    int sections;
-    int sectionNumber;
-    const int shelves = 6;
-    const int sequenceNumbers = 10;
+    Vector<Section> sections;
+	// logging class to be created and implemented
 
-    void copy(const Storage& rhs);
+	void fillShelves(int, const Product&, Vector<Shelf>&); // запълва рафтове с даден продукт
+	void addAndFillNewSections(int, const Product&, Vector<Section>&, const Date&); // добавя и запълва секции с даден продукт
+	const Date& createCurrDate() const; // създава дата, която е текущата за компютъра
 public:
     Storage();
+    // Storage(const Vector<Section>& sections, const Vector<Log>& logs) could be removed
     Storage(const Storage& rhs);
     Storage& operator=(const Storage& rhs);
     ~Storage();
 
     void displayInformation() const;
-    void addProduct();
-    void removeProduct();
+    void add(Vector<Section>&);
+    void removeProduct(Vector<Section>&);
     void referenceInformation(const Date& firstDate, const Date& secondDate);
-    void cleanUp(const Date& currentDate);
+    void cleanUp(Vector<Section>&);
 };
 
 #endif STORAGE_H
