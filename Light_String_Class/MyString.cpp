34d6f3149396aa7a98c4d74e95d0aa411998c845
Teodor Kehayov <56166346+Tedkouff@users.noreply.github.com>
2022-05-14 16:34:44 +0300

    Upload final version of needed string functionalities

--- Light_String_Class/MyString.cpp
+++ Light_String_Class/MyString.cpp
@@ -1,148 +1,181 @@
 #include "MyString.h"
 
 
-    MyString::MyString()  /// default constructor
+MyString::MyString()
+{
+    lenght=0;
+    str = new char[lenght + 1];
+    str[0] = '\0';
+}
+
+MyString::MyString(const char* str)
+{
+    lenght=strlen(str);
+    this->str = new char[lenght + 1];
+
+    strcpy(this->str,str);
+    this->str[lenght]='\0';
+}
+
+MyString::~MyString() { delete [] str; }
+
+MyString::MyString(const MyString& str)
+{
+    lenght = str.lenght;
+    this->str = new char[lenght+1];
+    strcpy(this->str,str.str);
+}
+
+MyString& MyString::operator= (const MyString& str1)
+{
+    if (this != &str1)
     {
-        lenght=0;
-        str = new char[lenght + 1];
-        str[0] = '\0';
+        delete str;
+        lenght = strlen(str1.str);
+        str = new char[lenght+1];
+        strcpy(str,str1.str);
+        str[lenght]='\0';
     }
 
-    /// създава нов обект и копира в него съдържанието на str
-    MyString::MyString(const char* str)
-    {
-        lenght=strlen(str);
-        this->str = new char[lenght + 1];
+    return *this;
+}
 
-        strcpy(this->str,str);
-        this->str[lenght]='\0';
+
+char& MyString::operator[](std::size_t pos)
+{
+    return str[pos];
+}
+const char& MyString::operator[](std::size_t pos) const
+{
+    return str[pos];
+}
+
+MyString &MyString::num_to_str(const int num)
+{
+    int br = 0, tempNum1 = num, tempNum2 = num;
+    while (tempNum1 > 0) {
+        tempNum1 /= 10;
+        br++;
+    }
+    char* numStr;
+    if (br == 0) {
+        numStr = new(std::nothrow) char[br + 2];
+        if (numStr == nullptr) {
+            throw std::runtime_error("No memory for string!(MyString::num_to_str())");
+        }
+    }
+    else numStr = new(std::nothrow) char[br + 1];
+    if (numStr == nullptr) {
+        throw std::runtime_error("No memory for string!(MyString::num_to_str())");
+    }
+    for (int i = 0; i < br; i++) {
+        numStr[br - i - 1] = tempNum2 % 10 + '0';
+        tempNum2 /= 10;
+    }
+    if (br == 0) {
+        numStr[0] = '0';
+        numStr[1] = '\0';
+    }
+    else numStr[br] = '\0';
+
+    *this = numStr;
+    delete[] numStr;
+
+    return *this;
+}
+
+std::size_t MyString::size() const
+{
+    return lenght;
+}
+
+void MyString::clear() {
+    delete[] str;
+    str = nullptr;
+    lenght = 0;
+}
+
+MyString& MyString::operator+=(const MyString& rhs)
+{
+    int temp = size() + rhs.size();
+    char* newStr = new char[temp + 1];
+
+    for (int i = 0; i < size(); i++)
+    {
+        newStr[i]=str[i];
+    }
+    int i,j=0;
+    for (i = size(); i < temp; i++)
+    {
+        newStr[i]=rhs[j];
+        j++;
     }
 
-    /// destructor
-    MyString::~MyString() { delete [] str; }
+    newStr[temp]='\0';
 
-    /// copy constructor
-    MyString::MyString(const MyString& str)
-    {
-        lenght = str.lenght;
-        this->str = new char[lenght+1];
-        strcpy(this->str,str.str);
-    }
+    *this = MyString(newStr);
+    delete [] newStr;
 
-    /// copy assignment operator
-    MyString& MyString::operator= (const MyString& str1)
+    return *this;
+}
+
+MyString MyString::operator+(const MyString& rhs) const
+{
+    int temp = size() + rhs.size();
+    char* newStr = new char[temp + 1];
+
+    strcpy(newStr,str);
+    strcat(newStr,rhs.str);
+    newStr[temp]='\0';
+
+    MyString tempStr{newStr};
+
+    delete [] newStr;
+    return tempStr;
+}
+
+bool MyString::operator== (const MyString& rhs) const
+{
+    if (strcmp(str, rhs.str) == 0)  return true;
+    return false;
+}
+
+bool MyString::operator== (const char* rhs) const
+{
+    if(strcmp(str,rhs) == 0) return true;
+    return false;
+}
+
+const char* MyString::c_str() const
+{
+    return str;
+}
+
+std::ostream& operator<< (std::ostream& os, const MyString& str)
+{
+    if (str.str)
     {
-        if (this != &str1)
+        for (int i = 0; i < str.lenght; ++i)
         {
-            delete str;
-            lenght = strlen(str1.str);
-            str = new char[lenght+1];
-            strcpy(str,str1.str);
-            str[lenght]='\0';
+            os<<str.str[i];
         }
+    }
+    return os;
+}
 
-        return *this;
+std::istream & operator>> (std::istream & is, MyString &str)
+{
+    char newStr[1024];
+    is >> newStr;
+
+    if (str.str) {
+        delete str.str;
+        str.str = nullptr;
     }
 
-    /// operator за достъп до елемент намиращ се на позиция pos
-    char& MyString::operator[](std::size_t pos)
-    {
-        return str[pos];
-    }
-    const char& MyString::operator[](std::size_t pos) const
-    {
-        return str[pos];
-    }
+    str.str = new char[strlen(newStr) + 1];
+    strcpy(str.str, newStr);
+    str.str[strlen(newStr)]= '\0';
 
-    /// дължина на низа
-    std::size_t MyString::size() const
-    {
-        return lenght;
-    }
-
-    MyString& MyString::operator+=(const MyString& rhs)
-    {
-        int temp = size() + rhs.size();
-        char* newStr = new char[temp + 1];
-
-        for (int i = 0; i < size(); i++)
-        {
-            newStr[i]=str[i];
-        }
-        int i,j=0;
-        for (i = size(); i < temp; i++)
-        {
-            newStr[i]=rhs[j];
-            j++;
-        }
-
-        newStr[temp]='\0';
-
-        *this = MyString(newStr);
-        delete [] newStr;
-
-        return *this;
-    }
-
-    MyString MyString::operator+(const MyString& rhs) const
-    {
-        int temp = size() + rhs.size();
-        char* newStr = new char[temp + 1];
-
-        strcpy(newStr,str);
-        strcat(newStr,rhs.str);
-        newStr[temp]='\0';
-
-        MyString tempStr{newStr};
-
-        delete [] newStr;
-        return tempStr;
-    }
-
-    /// проверява дали два символни низа са еднакви
-    bool MyString::operator== (const MyString& rhs) const
-    {
-        if (strcmp(str, rhs.str) == 0)  return true;
-        return false;
-    }
-
-    bool MyString::operator== (const char* rhs) const
-    {
-        if(strcmp(str,rhs) == 0) return true;
-        return false;
-    }
-
-    /// връща указател към null-terminated масив, който има съдържание идентично с това на низа
-    const char* MyString::c_str() const
-    {
-        return str;
-    }
-
-    std::ostream& operator<< (std::ostream& os, const MyString& str)
-    {
-        if (str.str)
-        {
-            for (int i = 0; i < str.lenght; ++i)
-            {
-                os<<str.str[i];
-            }
-        }
-        return os;
-    }
-
-    std::istream & operator>> (std::istream & is, MyString &str)
-    {
-        char newStr[1024];
-        is >> newStr;
-
-        if (str.str) {
-            delete str.str;
-            str.str = nullptr;
-        }
-
-        str.str = new char[strlen(newStr) + 1];
-        strcpy(str.str, newStr);
-        str.str[strlen(newStr)]= '\0';
-
-        return is;
-    }
+    return is;
+}
--- Light_String_Class/MyString.h
+++ Light_String_Class/MyString.h
@@ -13,24 +13,28 @@ private:
     std::size_t lenght;
 
 public:
-    MyString();
-    MyString(const char* str);
-    ~MyString();
-    MyString(const MyString& str);
-    MyString& operator= (const MyString& str);
-    char& operator[](std::size_t pos);
-    const char& operator[](std::size_t pos) const;
-    std::size_t size() const;
-    
-    MyString& operator+=(const MyString& rhs);
-    MyString operator+(const MyString& rhs) const;
-    const char* c_str() const;
-    bool operator==(const MyString &rhs) const;
-    bool operator== (const char* str) const;
+    MyString();                                     // default constructor
+    MyString(const char* str);                      // constructor with parameter
+    ~MyString();                                    // destructor
+    MyString(const MyString& str);                  // copy constructor
+    MyString& operator= (const MyString& str);      // copy assignment operator
 
+    char& operator[](std::size_t pos);              // оператор за достъп до елемент намиращ се на позиция pos
+    const char& operator[](std::size_t pos) const;  //
+    MyString& operator+=(const MyString& rhs);      // оператор за конкатенация на низове
+    MyString operator+(const MyString& rhs) const;  //
+    bool operator==(const MyString &rhs) const;     // оператор, проверяващ дали два символни низа са еднакви
+    bool operator== (const char* str) const;        //
+
+    MyString& num_to_str(const int num);            // връща число като символен низ
+    std::size_t size() const;                       // дължина на низа
+    void clear();                                   // изчиства съдържанието на низа
+    const char* c_str() const;                      // връща указател към null-terminated масив, който има съдържание
+                                                    // идентично с това на низа
+
+    // оператори за вход и изход
     friend std::ostream& operator<< (std::ostream& os, const MyString& str);
     friend std::istream& operator>> (std::istream& is, MyString &str);
 };
-
 
 #endif //MYSTRING_H
